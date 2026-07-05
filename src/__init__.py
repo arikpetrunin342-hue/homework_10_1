da2c0246fd9cea0e40aa7c9055e2b77e84976d30
Ярослав Петрунин <arikpetrunin342@gmail.com>
2026-07-05 14:01:25 +0300

    fix __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,9 @@
+from typing import List, Dict
+
 from src.processing import filter_by_state, sort_by_date
 
 # Исходные данные
-user_data: list[dict] = [
+user_data: List[Dict] = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}
 ]
 
